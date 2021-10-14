8993ca27994a52b7ba9eafcd0b67a33cc33b9a96
Andrew-van-Campen <andrewvancampen@gmail.com>
2021-10-14 19:05:33 -0400

    Edited the code and README file to make them more user-friendly.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # dynamic-wallpaper
 A Bash script that switches wallpaper in sync with sunrise and sunset times.
+
+### Installation Instructions
+1. Download the shell script file [dynamic-wallpaper.sh](https://github.com/Andrew-van-Campen/dynamic-wallpaper/blob/main/dynamic-wallpaper.sh) and save it to the location of your choice (right-click on "Raw" and click "Save Link As...").
+
+2. Make the script file executable using the command:
+> `chmod +x [some/path/]dynamic-wallpaper.sh`
+
+3. Open the script file with a text editor. You'll see a section of code labelled "`THIS SECTION OF CODE CAN BE EDITED BY THE USER`". Edit this section of code to set your location and your daytime and nighttime wallpapers, then save the file.
+
+4. Set the script to run every minute using cron.
+    1. First, ensure that cron is running using the command:
+    > `/sbin/service cron start`
+
+    2. Next, set the `EDITOR` environment variable to the text editor of your choice (Leafpad, in this example), as follows:
+    > `export EDITOR=leafpad`
+
+    3. Enter the following command to add a new cron task (it will open a file using your chosen text editor):
+    > `crontab -e`
+
+    4. Add the following line to the end of the file, then save the file and close the text editor:
+    > `* * * * * [some/path/]dynamic-wallpaper.sh`
+
+5. dynamic-wallpaper is now installed! If you change locations or wish to set new daytime and nighttime wallpapers, you'll have to make the appropriate edits to the script file.
--- dynamic-wallpaper.sh
+++ dynamic-wallpaper.sh
@@ -1,43 +1,49 @@
 #!/bin/bash
 
-#Set location.
-#location="CHXX0008" #Beijing, China
-#location="UKXX1428" #Greenwich, England
-location="CAXX0301" #Montreal, Canada
-#location="CAXX1026" #Almonte, Canada
+#----------------------------------------------------------------------------------------------------
+# THIS SECTION OF CODE CAN BE EDITED BY THE USER.
 
-#Path to storage file.
+# Set location. Location codes can be found at: https://weather.codes
+location="CAXX0301"  # Montreal, Canada
+
+# Set daytime and nighttime wallpapers.
+  daytime="/usr/share/backgrounds/odin.jpg"
+nighttime="/usr/share/backgrounds/odin-dark.jpg"
+
+#----------------------------------------------------------------------------------------------------
+
+# Path to storage file.
 storefile="/tmp/sunrise-sunset"
 
-#Function to retrieve sunrise and sunset times from weather.com, and store them in a file.
+# Function to retrieve sunrise and sunset times from weather.codes, and store them in a file.
 retrieveTimes()
 {
-    #Since wget stores the website data in a file, first delete any old files with this name to avoid an unexpected filename.
+    # Since wget stores the website data in a file, first delete any old files with this name to avoid an unexpected filename.
     tmpfile="/tmp/$location"
     if [ -f $tmpfile ]
     then
         rm $tmpfile
     fi
-    #Retrieve the website data using wget.
+    # Retrieve the website data using wget.
     wget -q "https://weather.com/weather/today/l/$location" -O $tmpfile
-    #Only continue if wget was successful.
+    # Only continue if wget was successful.
     if [ $? -eq 0 ]
     then
-        #Find the sunrise and sunset times in the website data.
+        # Find the sunrise and sunset times in the website data.
         sunrise_UTC=$(grep -o 'sunriseTimeUtc\\\":[0-9]*,' $tmpfile | cut -d : -f 2 | cut -d , -f 1)
         sunset_UTC=$(grep -o 'sunsetTimeUtc\\\":[0-9]*,' $tmpfile | cut -d : -f 2 | cut -d , -f 1)
-        #Store the location and sunrise and sunset times.
+        # Store the location and sunrise and sunset times.
         echo $location > $storefile
         echo $sunrise_UTC >> $storefile
         echo $sunset_UTC >> $storefile
     else
         echo "ERROR: Could not connect to https://weather.com/weather/today/l/$location"
     fi
-    #Delete the file containing the website data.
+    # Delete the file containing the website data.
     rm $tmpfile
 }
 
-#If the date or location has changed, or if the storage file doesn't exist, retrieve new sunrise and sunset times.
+# If the date or location has changed, or if the storage file doesn't exist, retrieve new sunrise and sunset times.
 if [ -f $storefile ]
 then
     if [ "$(date -d @$(cat $storefile | cut -d $'\n' -f 2) +%F)" != "$(date +%F)" ] || [ "$(cat $storefile | cut -d $'\n' -f 1)" != $location ]
@@ -45,20 +51,20 @@ then
         retrieveTimes
     fi
 else
-    #In case of both an unsuccessful wget and no previously-stored times to use, store some arbitrary location and times.
+    # In case of both an unsuccessful wget and no previously-stored times to use, store some arbitrary location and times.
     echo "UKXX1428" > $storefile
     echo "$((6 * 60 * 60))" >> $storefile
     echo "$((18 * 60 * 60))" >> $storefile
-    #If wget was successful, the above values will be overwritten.
+    # If wget is successful, the above values will be overwritten.
     retrieveTimes
 fi
 
-#Read sunrise and sunset times from file.
+# Read sunrise and sunset times from file.
 location=$(cat $storefile | cut -d $'\n' -f 1)
 sunrise_UTC=$(cat $storefile | cut -d $'\n' -f 2)
 sunset_UTC=$(cat $storefile | cut -d $'\n' -f 3)
 
-#Convert times to minutes so that date is not taken into account when comparing to current time.
+# Convert times to minutes so that date is not taken into account when comparing times.
 sunrise=$((60 * 10#$(date -d @$sunrise_UTC +%H) + 10#$(date -d @$sunrise_UTC +%M)))
 sunset=$((60 * 10#$(date -d @$sunset_UTC +%H) + 10#$(date -d @$sunset_UTC +%M)))
 if [ $sunrise -gt $sunset ]
@@ -66,20 +72,20 @@ then
     sunrise=$(($sunrise - 1440))
 fi
 
-#Set environment for cron.
+# Set environment for cron.
 PID=$(pgrep gnome-session)
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z 'DBUS_SESSION_BUS_ADDRESS' /proc/$PID/environ | cut -d = -f 2-999 | cut -d $'\0' -f 1)
 
-#Check the current time, and change the wallpaper accordingly.
+# Check the current time, and change the wallpaper accordingly.
 current_time=$((60 * 10#$(date +%H) + 10#$(date +%M)))
 if [ $current_time -gt $sunrise -a $current_time -lt $sunset ]
 then
-    gsettings set org.gnome.desktop.background picture-uri "/usr/share/backgrounds/odin.jpg"
+    gsettings set org.gnome.desktop.background picture-uri $daytime
 else
-    gsettings set org.gnome.desktop.background picture-uri "/usr/share/backgrounds/odin-dark.jpg"
+    gsettings set org.gnome.desktop.background picture-uri $nighttime
 fi
 
-#Function to print times.
+# Function to print times.
 printTimes()
 {
     echo "Location: $location"
